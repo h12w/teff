7f47495baf158624fefd2011bd1a0436d48170bb
Hǎiliàng Wáng <w@h12.me>
2015-06-05 16:12:39 +0800

    .

--- core/scan_test.go
+++ core/scan_test.go
@@ -38,20 +38,29 @@ func TestScan(t *testing.T) {
 		s        string
 		expected string
 	}{
+		{"", "<eof>"},
+		{" ", "<eof>"},
 		{"\r", "<eof>"},
 		{"\n", "<eof>"},
 		{"\r\n", "<eof>"},
 		{"\n\r", "<eof>"},
 		{"\n\n", "<eof>"},
+		{" \r\n ", "<eof>"},
+
 		{"x", "<x:s> <eof>"},
 		{"x\ny", "<x:s> <y:s> <eof>"},
 		{"#x", "<x:a> <eof>"},
 		{"#x\n#y", "<x:a> <y:a> <eof>"},
 
+		{"\nx", "<x:s> <eof>"},
+		{"  \nx", "<x:s> <eof>"},
+
 		{"x\n\ty", "<x:s> <in> <y:s> <un> <eof>"},
 		{"x\n\ty\n", "<x:s> <in> <y:s> <un> <eof>"},
 		{"x\n\ty\nz", "<x:s> <in> <y:s> <un> <z:s> <eof>"},
-		{"1\n\t2\n\t\t3\n\t\t\t4", "<1:s> <in> <2:s> <in> <3:s> <in> <4:s> <un> <un> <un> <eof>"},
+		{"1\n\t2\n\t\t3\n\t\t\t4\n5", "<1:s> <in> <2:s> <in> <3:s> <in> <4:s> <un> <un> <un> <5:s> <eof>"},
+
+		{"\tx\n\t\ty\nz", "<in> <x:s> <in> <y:s> <un> <un> <z:s> <eof>"},
 	} {
 		toks, err := scanAll(testcase.s)
 		if err != nil {
@@ -61,6 +70,13 @@ func TestScan(t *testing.T) {
 		if actual != testcase.expected {
 			t.Fatalf("testcase %d: expect\n%s\ngot\n%s\n", i, testcase.expected, actual)
 		}
+	}
+}
+
+func TestMismatch(t *testing.T) {
+	_, err := scanAll("x\n\ty\n x")
+	if err == nil {
+		t.Fatal("expect mismatch error")
 	}
 }
 
